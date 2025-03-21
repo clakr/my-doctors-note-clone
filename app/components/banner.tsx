5fb3ea2a94d84d4c921f19b14f61b01ab5fcd57e
clakr <clarktolosa@gmail.com>
2025-03-21 16:11:42 +0800

    feat(hero): created components

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -1,6 +1,6 @@
 export function Banner() {
   return (
-    <section className="bg-[#2323FD] py-3 text-center font-semibold text-white">
+    <section className="bg-vivid-blue py-3 text-center font-semibold text-white">
       30% OFF if you order in the next{" "}
       <strong className="font-bold">10:00</strong>
     </section>
--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,3 +1,34 @@
+import { ArrowRight } from "@/app/components/icons/arrow-right-icon";
+import { Secure } from "@/app/components/icons/secure-icon";
+import Image from "next/image";
+
 export function Hero() {
-    return <section>hero</section>
+  return (
+    <section className="relative isolate flex flex-col items-center gap-y-4 bg-[url(/hero.jpeg)] bg-cover bg-center px-4 py-16 text-center before:absolute before:inset-0 before:-z-10 before:bg-black/60">
+      <h1 className="mb-4 text-3xl font-bold text-white">
+        Your Doctor's Note In{" "}
+        <span className="relative whitespace-nowrap">
+          1 Minute, for $29,99!
+          <Image
+            src="/line-1.svg"
+            alt=""
+            width={250}
+            height={6}
+            className="absolute top-[calc(100%+.5rem)] left-4"
+          />
+        </span>
+      </h1>
+      <button
+        type="button"
+        className="bg-vivid-blue flex items-center justify-center gap-x-2 self-stretch rounded-xl p-4.5 font-semibold text-white"
+      >
+        Get your Note Now
+        <ArrowRight />
+      </button>
+      <span className="flex items-center justify-center gap-x-2 text-sm font-semibold text-white">
+        <Secure />
+        100% Risk-Free Money Back Guarantee
+      </span>
+    </section>
+  );
 }
